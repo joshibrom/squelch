c3836d62b385b16dfb6d89868fc8931d3733569a
Joshua Ibrom <joshuaibrom@gmail.com>
2026-04-07 21:04:18 -0500

    feat: store profile, render bold text
    Can store my profile information and render text in bold using a
    lightweight version of a Markdown parser (definitely doesn't handle edge
    cases).

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,61 @@
 const std = @import("std");
 
+const Link = struct {
+    title: []const u8,
+    href: []const u8,
+};
+
+const Profile = struct {
+    const Callsign = struct {
+        service: []const u8,
+        callsign: []const u8,
+    };
+    name: []const u8,
+    title: []const u8,
+    tagline: []const u8,
+    links: []const Link,
+    callsigns: []const Callsign,
+};
+
+const my_profile = Profile{
+    .name = "Joshua Ibrom",
+    .title = "Web Application Developer III & MSCS Student",
+    .tagline = "I build minimalist web infrastructure and explore **systems-level programming**. Currently focused on high-performing tooling with **Zig** and **Rust**.",
+    .links = &[_]Link{
+        .{ .title = "GitHub", .href = "https://github.com/joshibrom" },
+        .{ .title = "LinkedIn", .href = "https://linkedin.com/in/joshua-ibrom" },
+        .{ .title = "Email", .href = "mailto:joshuaibrom@gmail.com" },
+    },
+    .callsigns = &[_]Profile.Callsign{
+        .{ .service = "GMRS", .callsign = "WSIZ578" },
+    },
+};
+
+fn renderBolding(allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+    var output = std.ArrayList(u8).empty;
+    errdefer output.deinit(allocator);
+
+    var i: usize = 0;
+    var inside_bold = false;
+
+    while (i < input.len) {
+        if (i + 1 < input.len and std.mem.eql(u8, input[i .. i + 2], "**")) {
+            const tag = if (inside_bold) "</strong>" else "<strong>";
+            try output.appendSlice(allocator, tag);
+            inside_bold = !inside_bold;
+            i += 2;
+        } else {
+            try output.append(allocator, input[i]);
+            i += 1;
+        }
+    }
+
+    return output.toOwnedSlice(allocator);
+}
+
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    defer arena.deinit();
+    std.debug.print("{s}\n", .{try renderBolding(allocator, my_profile.tagline)});
 }
